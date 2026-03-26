045e4ab007b7a217525833474eded7b0c60cb74f
Garnn <80454749+Garnn@users.noreply.github.com>
2026-03-26 20:20:41 +0100

    dodałem prosty konstruktor dla cpmNode

--- graph.js
+++ graph.js
@@ -1,1 +1,12 @@
 console.log("Skrypt załadowany");
+
+class cpmNode {
+  constructor(time){
+    this.es = null
+    this.t = time
+    this.ef = null
+    this.ls = null
+    this.r = null
+    this.lf = null
+  }
+}
