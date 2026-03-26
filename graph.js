a755edc02a0c907d9f8d44630ea297c86f38d2fb
Garnn <80454749+Garnn@users.noreply.github.com>
2026-03-26 20:39:07 +0100

    dodałem prostą tablicę łączności i funkcję poszerzenia tablicy

--- graph.js
+++ graph.js
@@ -1,12 +1,34 @@
 console.log("Skrypt załadowany");
 
-class cpmNode {
-  constructor(time){
-    this.es = null
-    this.t = time
-    this.ef = null
-    this.ls = null
-    this.r = null
-    this.lf = null
+class conMatrix {
+  constructor(){
+    this.data = [[false]];
+    this.size = 1;
+  }
+
+  extendMatrix() {
+    for (let i = 0; i < this.data.length; i++) {
+      this.data[i].push(false);
+    }
+    this.size+=1;
+    let newRow = Array(this.size).fill(false);
+    this.data.push(newRow);
   }
 }
+
+class cpmNode {
+  constructor(time){
+    this.es = null;
+    this.t = time;
+    this.ef = null;
+    this.ls = null;
+    this.r = null;
+    this.lf = null;
+  }
+}
+
+const node = new cpmNode(10);
+const conns = new conMatrix();
+conns.extendMatrix();
+console.log(node);
+console.log(conns);
